a96bbed8b25ae15f71cd03aa9553e883860d11fd
melonSkin76 <grq2345@126.com>
2021-12-15 08:45:32 -0600

    Shrinkybrush for hand tracking done

---
+++ Final Project/Marching Cubes Pottery/Assets/Scripts/ShrinkyHands.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkyHands : MonoBehaviour
+{
+    public GameObject BrushTip;
+    public OVRSkeleton RightHandSkeleton;
+    Vector3 originalScale;
+
+    private bool isActivate;
+    private Transform indexTip;
+    private Transform thumbTip;
+    private Vector3 center;
+    private float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalScale = BrushTip.transform.localScale;
+
+        BrushTip.transform.localScale = Vector3.zero;
+        BrushTip.GetComponent<MeshRenderer>().enabled = true;
+        BrushTip.GetComponent<SphereCollider>().enabled = true;
+
+        isActivate = false;
+        BrushTip.SetActive(isActivate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActivate == false && OVRInput.Get(OVRInput.Button.One))
+        {
+            isActivate = true;
+            Debug.Log("ShrinkyHands activatied");
+        } else if (isActivate && OVRInput.Get(OVRInput.Button.One))
+        {
+            isActivate = false;
+            Debug.Log("ShrinkyHands deactivatied");
+        }
+
+        if (OVRPlugin.GetHandTrackingEnabled() && isActivate)
+        {
+            BrushTip.SetActive(true);
+
+            bonesHandler();
+
+            foreach (var bone in RightHandSkeleton.Bones)
+            {
+                if (bone.Id == OVRSkeleton.BoneId.Hand_IndexTip)
+                {
+                    indexTip = bone.Transform;
+                } else if (bone.Id == OVRSkeleton.BoneId.Hand_ThumbTip)
+                {
+                    thumbTip = bone.Transform;
+                }
+            }
+
+            //Debug.Log("!!!!!!!!!!!!!!!!!!!!!!!!");
+            //Debug.Log(indexTip.position);
+            //Debug.Log(thumbTip.position);
+            //Debug.Log("!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            center = (indexTip.position + thumbTip.position) / 2;
+            radius = Vector3.Distance(indexTip.position, thumbTip.position) / 2;
+
+            BrushTip.transform.position = center;
+            BrushTip.transform.localScale = new Vector3(radius * 2, radius * 2, radius * 2);
+        } else
+        {
+            BrushTip.SetActive(false);
+        }
+    }
+
+    void ChangeBrushSize()
+    {
+        if (this.enabled)
+        {
+            BrushTip.transform.localScale = originalScale;
+        }
+    }
+
+    void ResetBrushSize()
+    {
+        if (this.enabled)
+        {
+            BrushTip.transform.localScale = originalScale;
+        }
+    }
+
+    private void OnDisable()
+    {
+        BrushTip.GetComponent<MeshRenderer>().enabled = false;
+        BrushTip.GetComponent<SphereCollider>().enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        BrushTip.GetComponent<MeshRenderer>().enabled = true;
+        BrushTip.GetComponent<SphereCollider>().enabled = true;
+    }
+
+    IEnumerator bonesHandler()
+    {
+        while (RightHandSkeleton.Bones.Count == 0)
+        {
+            yield return null;
+        }
+    }
+}
